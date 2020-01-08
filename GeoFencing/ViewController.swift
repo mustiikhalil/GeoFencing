74e2d1ee4ca26bd5b397d2a341c6bb67a3243621
mustii <mustii@mmk.one>
2020-01-08 17:40:12 +0300

    Initial Commit

---
+++ GeoFencing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoFencing
+//
+//  Created by Mustafa Khalil on 1/8/20.
+//  Copyright © 2020 Mustafa Khalil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
